9051f6d73b934c90f845e25cebd83b4bd4825de0
Jirayut Khantavee <jirayut.kh@kbtg.tech>
2019-06-13 09:21:01 +0700

    Modify dependency

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import stellarRouter from './stellar'
-import '@babel/polyfill'
 
 const app = express()
 const port = process.env.PORT || 3000
